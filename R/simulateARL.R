ad21fdf0f0095ed02504aa82101fd4558f7e0b00
chindhanai <chindhanaipom@gmail.com>
2017-07-14 04:14:11 -0700

    Update the help file. To be completed by Tom

--- R/simulateARL.R
+++ R/simulateARL.R
@@ -1,80 +1,50 @@
-#' @title Using Statistical Process Control to Monitor Active Management
+#' @title Simulation for thresholds of the Lindley's recursion
 #'
-#' @description Compute the log-likelihood ratio given the performance measure,
-#' which is the information ratios of the active managers' portfolios, to gauge
-#' the performance of the active managers with respect to specific thresholds.
-#' An object of class \code{"tsfm"} is returned.
+#' @description This function simulates thresholds of Lindley's recursion
+#' used in the function \code{cusumActMgr}.
 #'
 #' @details
-#' It is hard to assess the performance of the active managers. Thus investors have to continually
-#' estimate the current performance of their portfolios and rigorously reevaluate
-#' each manager’s strategy. Change point detection, such as CUSUM, is closely related to the dynamic performance
-#' measurement in sequential analysis and statistical process control (SPC), and
-#' the sequential anaysis can speed up the detection of change in systems.
-#'
-#' CUSUM is a procedure to rapidly detect the changes in the mean of a noisy random
-#' process and it is robust to the distribution of the portfolio returns. CUSUM offers
-#' the user the fastest way to detect a change in performance and the best efficiency
-#' over the Wald’s sequential probability ratio test (SPRT) and t-test of both stationary
-#' performance and time-varying performance. In this case, IR is used as a measure of
-#' performance as it incorporates both risk (portfolio’s tracking error) and return
-#' (portfolio’s excess return relative to the benchmark). Also, IR variance
-#' is lower than that of the Sharpe ratio and the Trynor ratio.
 #'
 #'
-#' \subsection{Data Processing}
+#' @param mu A numeric value representing the information ratio we want to
+#' simulate thresholds for. No default value is set.
+#' @param Threshold A numeric value representing the threshold for the Lindley's
+#' recursion to be updated in the recursion. No default value is set.
+#' @param delta A numeric value representing the simulation accuracy.
+#' In other words, we will simulate until 3 * sigma / mu < precision.
+#' @param EW_constant A numeric value representing the ratio between the former sigma
+#' and the new sigma. The default is set to 0.9
+#' @param Fixed_Sigma The logical value representing if the sigma should be constant of not.
+#' 0 represents fixed sigma, 1 represents weighted sigma. The default is set to 1.
 #'
-#' @param portfolioName a character representing the name of the fund.
-#' It is a required argument with no default
-#' @param benchmarkName a character representing the name of the benchamark
-#' It is a required argument with no default
-#' @param data an xts object containing the columns \code{portfolioName} and
-#' \code{benchmarkName} of monthly returns. This argument is required with no default.
-#' @param upperIR a numeric value representing the information ratio of a
-#' good performance. The default is set to 0.5
-#' @param lowerIR a numeric value representing the information ratio of a
-#' bad performance. The default is set to 0
-#' @param lambda_in the exponential weighting constant when the data
-#'  seems consistent with the current estimate of volatility.
-#' The default is set to 0.1
-#' @param lambda_out the exponential weighting constant when the data
-#' seems inconsistent with the current estimate of volatility.
-#' The default is set to 0.2
-#' @param winsorize the numeric value, greater than 1, of standard deviations
-#' at which we winsorize. The default is set to 4.
-#' @param filterStd the logical value representing the filter of the estimated
-#' standard deviations. The default is set to \code{FALSE}.
+#' @return \code{simulateARL} returns a \code{vector} of the following:
+#' \item{ARL}{The average return length}
+#' \item{s}{The standard deviation of the ARLs}
 #'
-#'
-#' @return \code{cusumActMgr} returns a \code{list} containing the following xts objects:
-#' \item{Logarithmic_Excess_Returns}{Logarithmic excess returns of
-#' the fund relative to the benchmark}
-#' \item{Annual_Moving_Average}{The vector of annual moving average returns}
-#' \item{Tracking_Error}{The monthly tracking error of the logarithmic excess returns}
-#' \item{Information_Ratios}{The vector of monthly information ratios}
-#' \item{Lindley's_Recursion}{The vector  Lindley's recursion with a reset after the detection threshold (6.81) is passed.}
-#' \item{Annualized_Cusum_IR}{The vector annualized CUSUM of the information ratios}
-#' \item{Means}{The xts matrix of estimated means of the fund in the first columns,
-#' the benchmark in the second column, and the excess logarithmic returns in the third column}
-#' \item{Standard_deviations}{The xts matrix of estimated standard deviations of the fund in the first columns,
-#' the benchmark in the second column, and the excess logarithmic returns in the third column.
-#' It will not be filtered unless \code{filterStd = TRUE} is specified.}
-#' \item{Protractor}{The xts matrix of the rays from IR = -3 in the first column
-#' to IR = 3 in the seventh column used in the CUSUM IR as a protractor.}
-#' \item{Excess_Volatility}{The annualized Standard deviations}
-#'
-#' @author Chindhanai Uthaisaad.
+#' @author Thomas Philips, Chindhanai Uthaisaad.
 #'
 #' @references
 #' Philips, T. K., Yashchin, E. and Stein, D. M. (2003). “Using Statistical
 #' Process Control to Monitor Active Managers”, Journal of Portfolio Management,
 #' Fall 2003, pp. 86-94.
+#'
+#' @example
+#' M = 100
+#' Lower_Threshold = 1.00
+#' Upper_Threshold = 11.0
+#' #Monthly mu's for monthly sigma=1, annualized IR = +0.5, 0
+#' mu = c(0.5, 0) / sqrt(12)
+#' Seq_M = 0:M
+#' Thresholds = Lower_Threshold + Seq_M * (Upper_Threshold - Lower_Threshold) / M
+#' Threshold_upper = sapply(Thresholds, FUN = simulateARL, mu = mu[1], delta = 0.1)
+#' Threshold_lower = sapply(Thresholds, FUN = simulateARL, mu = mu[2], delta = 0.1)
+#'
 #' @export
 
 ############################< MAIN CODE >###############################
 # This function simulates the threshold for Lindley's recursion
 
-simulateARL = function(mu, Threshold, delta, h, EW_constant, Fixed_Sigma){
+simulateARL = function(mu, Threshold, delta, EW_constant = 0.9, Fixed_Sigma = 1){
   N_Events          = 0
   Sum               = 0
   SumSq             = 0
@@ -100,7 +70,7 @@ simulateARL = function(mu, Threshold, delta, h, EW_constant, Fixed_Sigma){
       IR_hat    = r_n / sigma_n_1
       sigma_n_1 = sigma_n
       r_n_1     = r_n
-      L         = max(0, L - IR_hat + h)
+      L         = max(0, L - IR_hat + mean(mu))
       N         <- N + 1
     }
 
