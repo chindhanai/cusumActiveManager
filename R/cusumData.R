d5c1526049042dac8c9910452f059b250a777563
chindhanai <chindhanaipom@gmail.com>
2017-07-14 04:54:40 -0700

    Add help file for cusumData

---
+++ R/cusumData.R
@@ -0,0 +1,8 @@
+#' The dataset 'cusumData' in cusumActiveManager package
+#'
+#' @description Data in the example is an xts object containing two monthly returns in each column.
+#' The first column is the fund returns and the second column is the
+#' benchmark returns. The data is from Jan 2005 to Dec 2003.
+#' @name cusumData
+#'
+NULL
