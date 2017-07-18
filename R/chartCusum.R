9ffa770955514acd80f3b7b53533bf8c24fa5297
chindhanai <chindhanaipom@gmail.com>
2017-07-17 17:03:12 -0700

    Reorder the graphs

--- R/chartCusum.R
+++ R/chartCusum.R
@@ -21,9 +21,6 @@
 #' of slopes with corresponding IR, and a plot of Lindley's Recursion with
 #' the thresholds\cr
 #' 3 = Scatter plot between the fund and benchmark returns with robust regression \cr
-#' @param layouts A list containing two vectors. The first vector corresponds to
-#' the layout of 'which = 1', and the second vector corresponds to that of 'which = 2'.
-#' Default is list(c(2,2), c(1,3))
 #' @param ... other graphics parameters in plot
 #'
 #' @return
@@ -40,8 +37,7 @@
 #' @export
 
 
-chartCusum <- function(object, digits = 3, which = NULL,
-                       layouts = list(c(2,2), c(1,3)), ...) {
+chartCusum <- function(object, digits = 3, which = NULL, ...) {
 
     options(digits = digits)
 
@@ -53,9 +49,9 @@ chartCusum <- function(object, digits = 3, which = NULL,
              "1L" = {
                # 1: Plot of log-excess returns with annually moving average returns
                P1 = xyplot(100 * object$Logarithmic_Excess_Returns, ylab = "Excess Return",
-                           main = "Montly Excess Return",
+                           main = "Monthly Excess Return",
                            horizontal = FALSE, col = "blue", type = c("h", "g"), lwd = 3,
-                           scales = list(y = list(rot = 0)),
+                           scales = list(y = list(rot = 0)), xlab = "",
                            panel = function(x, y,...) {
                             panel.xyplot(x, y,...)
                             panel.lines(100 * object$Annual_Moving_Average,
@@ -65,14 +61,14 @@ chartCusum <- function(object, digits = 3, which = NULL,
                                      lines = list(col = 2, lty = 1, lwd = 2),
                                      text = list("Moving Average"),
                                      cex = 0.4))
-               # 2: Plot of tracking error
-               P2 = xyplot(100*as.zoo(sqrt(12)*object$Tracking_Error),
+               # 3: Plot of tracking error
+               P3 = xyplot(100*as.zoo(sqrt(12)*object$Tracking_Error),
                           main="Annualized Tracking Error", type = 'l', las=0,
                           xlab = "", ylab = "%", col = 4, lwd = 1.5,
                           scales = list(y = list(rot = 0)))
-               # 3: Excess volatility plot
-               P3 = xyplot(as.zoo(100*object$Excess_Volatility[,3]),
-                          main = "Excess Volatility Relative to Benchmark", las=2, col=4,
+               # 4: Excess volatility plot
+               P4 = xyplot(as.zoo(100*object$Excess_Volatility[,3]),
+                          main = "Excess Volatility", las=2, col=4,
                           xlab = "", ylab = "%", lwd = 1.5,
                           scales = list(y = list(rot = 0)),
                           panel=function(x,y,...){
@@ -80,10 +76,10 @@ chartCusum <- function(object, digits = 3, which = NULL,
                             panel.abline(h = 0, col = 4, lty = 3)})
                colors = c("firebrick4", "firebrick3", "firebrick2", 1,
                           "green4", "green3", "green2")
-               # 4: CUSUM for returns
-               P4 = xyplot(as.zoo(object$Annualized_Cusum_ER),
-                          main="CUSUM Plot: Annualized Excess Returns",
-                          las=2, col=4, ylab = "", lwd = 1.5,
+               # 2: CUSUM for returns
+               P2 = xyplot(as.zoo(object$Annualized_Cusum_ER),
+                          main="Cusum Excess Returns",
+                          las=2, col=4, ylab = "", lwd = 1.5, xlab = "",
                           scales = list(y = list(rot = 0)),
                           panel=function(x,y,...){
                             panel.xyplot(x,y,...)
@@ -98,31 +94,17 @@ chartCusum <- function(object, digits = 3, which = NULL,
                                                  "ER = 1%","ER = 2%","ER = 3%")),
                                    title = "Slopes on Protractor\n Excess Returns",
                                    cex = 0.25))
-             if (identical(layouts[[1]], c(2,2))) {
+
                print(P1, split = c(1, 1, 2, 2), more = TRUE)
-               print(P2, split = c(1, 2, 2, 2), more = TRUE)
-               print(P3, split = c(2, 1, 2, 2), more = TRUE)
+               print(P2, split = c(2, 1, 2, 2), more = TRUE)
+               print(P3, split = c(1, 2, 2, 2), more = TRUE)
                print(P4, split = c(2, 2, 2, 2))
-             } else if (identical(layouts[[1]], c(4,1))) {
-               print(P1, split = c(1, 1, 4, 1), more = TRUE)
-               print(P2, split = c(2, 1, 4, 1), more = TRUE)
-               print(P3, split = c(3, 1, 4, 1), more = TRUE)
-               print(P4, split = c(4, 1, 4, 1))
-             } else if (identical(layouts[[1]], c(1,4))) {
-               print(P1, split = c(1, 1, 4, 1), more = TRUE)
-               print(P2, split = c(1, 2, 4, 1), more = TRUE)
-               print(P3, split = c(1, 3, 4, 1), more = TRUE)
-               print(P4, split = c(1, 4, 4, 1))
-             } else {
-               return("Invalid argument: layout of which = 1 is incorrect")
-               break
-             }
 
              },
              "2L" = {
                # 1: Plot of IR
                P1 = xyplot(as.zoo(object$Information_Ratios),
-                          main="Estimated Information Ratios",
+                          main="Estimated IR",
                           col=4, las=1, horizontal = FALSE, type = 'h',
                           ylab = "IR", lwd = 1.75,
                           scales = list(y = list(rot = 0)),
@@ -133,7 +115,7 @@ chartCusum <- function(object, digits = 3, which = NULL,
                           "green4", "green3", "green2")
                # 2: cusumIR
                P2 = xyplot(as.zoo(object$Annualized_Cusum_IR),
-                          main="CUSUM Plot: Estimated Information Ratio",
+                          main="Cusum Estimated IR",
                           col=4, lwd=1.5, horizontal = FALSE,
                           scales = list(y = list(rot = 0)),
                           panel=function(x,y,...){
@@ -148,8 +130,8 @@ chartCusum <- function(object, digits = 3, which = NULL,
                                    text = list(c("IR = -3","IR = -2","IR = -1", "IR = 0",
                                                  "IR = 1","IR = 2","IR = 3")),
                                    title = "Slopes on Protractor",
-                                   cex = 0.2)
-               )
+                                   cex = 0.2))
+
                horiz = c(-3.41, -4.33, -5.08, -5.72, -6.29, -6.81)
                colors2 = c(3, "green3", 7, "goldenrod1", "orangered", 2)
                ycoor = c(-3.3, -4.2, -4.9, -5.6, -6.2, -6.7)
@@ -171,19 +153,9 @@ chartCusum <- function(object, digits = 3, which = NULL,
                               panel.text(as.yearmon('2005-06', "%Y-%m"), y = ycoor[i], thresholds[i], cex = 0.4)
                             }
                           })
-               if (identical(layouts[[2]], c(1,3))) {
-                 print(P1, split = c(1, 1, layouts[[2]]), more = TRUE)
-                 print(P2, split = c(1, 2, layouts[[2]]), more = TRUE)
-                 print(P3, split = c(1, 3, layouts[[2]]))
-               } else if (identical(layouts[[2]], c(3,1))) {
-                 print(P1, split = c(1, 1, layouts[[2]]), more = TRUE)
-                 print(P2, split = c(1, 2, layouts[[2]]), more = TRUE)
-                 print(P3, split = c(1, 3, layouts[[2]]))
-               } else {
-                 return("Invalid argument: layout of which = 2 is incorrect")
-                 break
-               }
-
+               print(P1, split=c(1,1,2,2), more=TRUE)
+               print(P2, split=c(2,1,2,2), more=TRUE)
+               print(P3, position = c(.25,0,0.75,0.5))
              },
              "3L" = {
                #Scatter plot with robust regression
